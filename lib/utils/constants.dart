db3f03150c483200dc2d127220f5b53021e2b3ac
tarun470 <duggempuditarun6@gmail.com>
2025-10-19 16:44:59 +0530

    Change API and socket URLs to production
    Updated API and socket URLs to production endpoints.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const String apiUrl = "http://localhost:5000/api";
-  static const String socketUrl = "http://localhost:5000";
+  // API & socket URLs
+  static const String apiUrl = "https://flutter-backend-xyz.onrender.com/api";
+  static const String socketUrl = "https://flutter-backend-xyz.onrender.com";
 
+  // Colors
   static const Color primary = Color(0xFF00FFF0);
   static const Color accent = Color(0xFFFF00FF);
   static const Color background = Colors.black;
