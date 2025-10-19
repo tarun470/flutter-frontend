61204527a3faa00ff08015a8b4cc5e31798b814e
tarun470 <duggempuditarun6@gmail.com>
2025-10-19 17:13:55 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Constants {
   // API & socket URLs
-  static const String apiUrl = "https://flutter-backend-xyz.onrender.com/api";
-  static const String socketUrl = "https://flutter-backend-xyz.onrender.com";
+  static const String apiUrl = "https://chat-backend-mnz7.onrender.com/api";
+  static const String socketUrl = "https://chat-backend-mnz7.onrender.com";
 
   // Colors
   static const Color primary = Color(0xFF00FFF0);
