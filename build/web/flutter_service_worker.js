54565367c6fab0324f6a9e62fbf43e6fd7c86039
tarun470 <duggempuditarun6@gmail.com>
2025-10-20 10:46:21 +0530

    Add/Update Flutter web build folder

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "74a0546a3ba4fbee4a3002d479a96346
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5364ecf31e6b20a0787990936921e3d3",
+"flutter_bootstrap.js": "19362374a551e5d23b919aa1e06ad2ca",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
